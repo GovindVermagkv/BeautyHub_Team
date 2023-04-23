3f71954cdc5f76a9cd1edc95a671f2687f57aa06
GovindVermagkv <sonygovind.gkv740@gmail.com>
2023-04-24 00:14:19 +0530

    structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Components/user_panel/layout/header';
+import Home from './Components/user_panel/views/Home';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Home />
     </div>
   );
 }
